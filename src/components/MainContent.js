f777c7bba41211276444ac6d8aa31d64681eb55f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-25 13:27:18 -0400

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,7 +3,7 @@ import React from "react";
 function MainContent() {
     const firstname = "Juan"
     const lastname = "Cruz"
-    return <p className="maincontent">This is the main content</p>;
+    return <p className="maincontent">DEMETRIUS doesn't know much</p>;
 }
 
 export default MainContent;
