4a86be7c555787f76030dff7fa756d91f7881f8e
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-24 21:40:51 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <header className="">This is the footer </header>;
+  return <header className="">This is the Header </header>;
 }
 
 export default Header;
