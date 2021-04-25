e636e9084e0c6e3aeb3d56a85840afe2ee59cd9a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-24 21:52:14 -0400

    add css navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <header className="">This is the Header </header>;
+  return <header className="navbar">This is the Header </header>;
 }
 
 export default Header;
