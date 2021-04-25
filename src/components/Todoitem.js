be055253d7d24d0685b5889e11ba3c8dd4e853a0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-25 13:32:28 -0400

    Update Todoitem.js
    create Todoitem function

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,7 +1,16 @@
+import React from 'react';
 
 
 
+function TodoItem() {
+    return (
+        <input type="checkbox" />
+<p>Placeholder text here</p>
+    )
 
+}
+
+export default TodoItem;
 // <input type="checkbox" />
 // <p>Placeholder text here</p>
 
