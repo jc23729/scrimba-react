390a532b99641f73a61f1f894f9638d63a4f1366
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-25 13:30:20 -0400

    Create Todoitem.js

---
+++ src/components/Todoitem.js
@@ -0,0 +1,15 @@
+
+
+
+
+// <input type="checkbox" />
+// <p>Placeholder text here</p>
+
+// <input type="checkbox" />
+// <p>Placeholder text here</p>
+
+// <input type="checkbox" />
+// <p>Placeholder text here</p>
+
+// <input type="checkbox" />
+// <p>Placeholder text here</p>
